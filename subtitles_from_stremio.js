868d9b5b58ec1dd989b01363987bbd0df0ce8dfb
m3dfatboi <142651452+m3dfatboi@users.noreply.github.com>
2025-08-14 17:34:36 +0300

    Update subtitles_from_stremio.js

--- subtitles_from_stremio.js
+++ subtitles_from_stremio.js
@@ -1,35 +1,14 @@
 (function () {
     'use strict';
 
-    var plugin = {
-        name: 'SubHero Subtitles for Lampa',
-        version: '1.0',
-        description: 'Загружает русские субтитры из Stremio SubHero для торрентов',
+    Lampa.Plugin.add({
+        plugin_name: 'test_subtitles',
+        name: 'Test Subtitles Plugin',
+        description: 'Простой тест плагина для субтитров',
+        version: '1.0.0',
         init: function () {
-            Lampa.Player.listen('start', this.loadSubtitles.bind(this));
-        },
-        loadSubtitles: function (data) {
-            if (data.source !== 'torrent') return; // Только для торрентов
-
-            var movieName = data.title || ''; // Название для поиска
-            var lang = 'ru'; // Русский язык (измени на 'eng' или другой, если нужно)
-
-            // Запрос к эндпоинту SubHero (адаптировано под Stremio-формат)
-            var subheroUrl = 'https://subhero.onrender.com/subtitles/movie/' + encodeURIComponent(movieName) + '.json?lang=' + lang;
-
-            Lampa.Network.request({
-                url: subheroUrl,
-                method: 'GET',
-                success: function (response) {
-                    if (response.subtitles && response.subtitles.length > 0) {
-                        var subUrl = response.subtitles[0].url; // Берём первый подходящий SRT
-                        Lampa.Player.subtitle.add({ url: subUrl, lang: lang, label: 'SubHero (RU)' });
-                    }
-                },
-                error: function () {} // Пропускаем ошибки молча
-            });
+            Lampa.Noty.show('Тестовый плагин загружен успешно!');
         }
-    };
+    });
 
-    Lampa.Plugin.add(plugin);
 })();
